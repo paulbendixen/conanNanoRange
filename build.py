745af91207c4758fa5634467df86ac2af2eb0db2
Paul M. Bendixen <paulbendixen@gmail.com>
2019-10-01 12:36:04 +0200

    Updated build.py to hopefully use cppstd=gnu17

--- build.py
+++ build.py
@@ -4,4 +4,6 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds()
+    for bld in builder.builds:
+        bld.settings["compiler.cppstd"] = "gnu17"
     builder.run()
