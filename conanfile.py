85dbebdd4f07165b0fe3dfbfdd7756bd54065352
Paul M. Bendixen <paulbendixen@gmail.com>
2019-11-06 12:09:06 +0100

    Updated to align with bincrafters

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,19 @@
 import os
+import glob
 
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 
 
 class NanorangeConan(ConanFile):
-    name = "nanoRange"
-    version = "0.1"
-    license = "Boost 1.0"
+    name = "nanorange"
+    version = "20191001"
+    license = "MIT"
     author = "Paul M. Bendixen paulbendixen@gmail.com"
     url = "gitlab.com/paulbendixen/conannanorange"
+    website = "https://github.com/tcbrindle/NanoRange"
     description = "NanoRange is a C++17 implementation of the C++20 Ranges proposals (formerly the Ranges TS)."
-    topics = ("ranges", "C++17", "Ranges TS")
+    topics = ("conan", "NanoRange", "ranges", "C++17", "Ranges TS")
     no_copy_source = True
     settings = "compiler"
     # No settings/options are necessary, this is header only
@@ -21,10 +23,13 @@ class NanorangeConan(ConanFile):
             raise ConanInvalidConfiguration("nanoRange requires at least c++17")
 
     def source(self):
-        self.run("git clone https://github.com/tcbrindle/NanoRange.git")
+        tools.get("https://github.com/tcbrindle/NanoRange/archive/abc34279d71369527e4da43cc4d02cf77446f79a.tar.gz", destination="source_folder", sha256="799431094ab784eb3cdbcd6de6681ff5d8b2b28e1a1e26e0a3a94fbfbfcd7e75" ) 
 
     def package(self):
-        self.copy("*.hpp", src="NanoRange/include", dst="include" )
+        sourceSubfolder = glob.glob("{}/source_folder/NanoRange-*".format(self.source_folder))[0]
+        print( sourceSubfolder )
+        self.copy("*.hpp", src="{}/include".format(sourceSubfolder), dst="include" )
+        self.copy("LICENSE_1_0.txt", src=sourceSubfolder, dst="licenses")
 
     def package_id(self):
         self.info.header_only()
